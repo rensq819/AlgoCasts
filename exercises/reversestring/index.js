3c8c35ec3a291a1d5fd2147fcfb3566dac58b001
Siqi Ren <sren@unitedsolutions.local>
2019-06-03 17:13:02 -0400

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,39 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/** Solution 1 */
+// function reverse(str) {
+//   const arr = str.split(''); // take string to array
+//   arr.reverse(); // reverse the array
+//   return arr.join(''); // join the letters into string
+// }
+
+/** Improved Solution 1 */
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+/** Solution 2 */
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+/** Solution 3 */
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
+
+/** Improved Solution 3 */
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
