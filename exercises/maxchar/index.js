a3a55de312db3cfa68475591cb1323993d5b0f30
Siqi Ren <siqi.ren@gmail.com>
2019-06-04 13:51:03 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) { // for ARRAY, STRING, use for (let ... OF ...)
+    // obj[char] ? obj[char]++ : (obj[char] = 1);
+    obj[char] = obj[char] + 1 || 1; // if obj[char] is undefined, assign 1
+  }
+
+  for (let char in obj) { // for OBJECT, use for (let ... IN ...)
+    if (obj[char] > max) {
+      max = obj[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
