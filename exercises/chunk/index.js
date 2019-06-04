c290e1812701aadd218ad7761bf8728e9be2de61
Siqi Ren <siqi.ren@gmail.com>
2019-06-04 15:44:53 -0400

    chunked array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+/** Solution 1 */
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1]; // fetch the last array of chunkced
+
+//     if (!last || last.length === size) { // if last array doesn't exist or last array lengh is equal to size
+//       chunked.push([element]); // chunked create new array
+//     } else {
+//       last.push(element); // last array push new element
+//     }
+//   }
+//   return chunked; // return chucnked
+// }
+
+/** Solution 2 */
+function chunk(array, size) {
+  const chunked = [];
+  let start = 0;
+
+  while (start < array.length) {
+    chunked.push(array.slice(start, start + size)); // slice return a new array but does NOT change the original array
+    start += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
