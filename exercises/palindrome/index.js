9e17f2c2dc0e0fac1bb54e9215a3bc641ecc4469
Siqi Ren <siqi.ren@gmail.com>
2019-06-04 11:43:23 -0400

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,23 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+/** Solution 1 */
+// function palindrome(str) {
+//   return str.split('').reverse().join('') === str;
+// }
+
+/** Solution 2 */
+// function palindrome(str) {
+//   return str.split('').reduce((rev, char) => {
+//     return char + rev;
+//   }, '') === str;
+// }
+
+/** Solution 3 */
+function palindrome(str) {
+  return str.split('').every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
+}
 
 module.exports = palindrome;
