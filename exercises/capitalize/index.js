0b60e8a459846fa06a81e44a44fbc94fd639161c
Siqi Ren <siqi.ren@gmail.com>
2019-06-04 17:27:14 -0400

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,29 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+/** Solution 1 */
+// function capitalize(str) {
+//   let arr = [];
+//   for (let word of str.split(' ')) { // split sentence by space, into words array, iterate the array
+//     word = word[0].toUpperCase().concat(word.slice(1)); // take the first letter and capitalize, concat the capitalized letter back with the rest of the word
+//     arr.push(word); // pushed the new word into array
+//   }
+//   return arr.join(' '); // join back the array with space.
+// }
+
+/** Solution 2 */
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
