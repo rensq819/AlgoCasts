47a3bf78d1206a69b43ade5848cd7d403f8464a1
Siqi Ren <siqi.ren@gmail.com>
2019-06-04 16:36:15 -0400

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/** Solution 1 */
+// function anagrams(stringA, stringB) {
+//   const mapA = buildMap(stringA); // use the hashmap helper to reduce duplicates
+//   const mapB = buildMap(stringB);
+
+//   if (Object.keys(mapA).length !== Object.keys(mapB).length) { // if keys length are not equal, return false immedietaly
+//     return false;
+//   } else { // if keys length are same, compare values
+//     for (let char in mapA) {
+//       if (!mapB[char] || mapB[char] !== mapA[char]) { // if value doesn't exist on one or is not the same, return false
+//         return false;
+//       }
+//     }
+//   }
+
+//   return true;
+// }
+
+// function buildMap(str) {
+//   const map = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     map[char] = map[char] + 1 || 1;
+//   }
+
+//   return map;
+// }
+
+/** Solution 2 */ /** clean out punctuation and space, all lower case, split into array and sort, join back to string */
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
 
 module.exports = anagrams;
