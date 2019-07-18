82d3bb74ad5f6d70adc9c8498ef7f9ec83eb31ef
Siqi Ren <siqi.ren@gmail.com>
2019-07-18 13:58:31 -0400

    use regex for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+/** Solution 1 */
+// function vowels(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+//   let count = 0;
+//   for(let char of str.toLowerCase()){
+//     if(vowels.includes(char)){
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+/** Solution 2 */
+// use regex 
+function vowels(str) {
+  // check for anything that's inside "[]" the bracket, g makes sure that we don't stop for only the first match find, i for case insensitive
+  const matches = str.match(/[aeiou]/gi); // see if some char is included in the string. 
+  // matches will be either an array or null
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
