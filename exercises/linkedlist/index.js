e37eb63835bba6c6e3ce72c328aa449a30a0eaf2
Siqi Ren <siqi.ren@gmail.com>
2020-01-09 21:17:40 -0500

    size, getFirst and getLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -17,6 +17,28 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data, this.head);
   }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+    while (node && node.next) {
+      node = node.next;
+    }
+    return node;
+  }
 }
 
 module.exports = { Node, LinkedList };
