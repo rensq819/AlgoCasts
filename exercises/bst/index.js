9f9dd308a508d6837c10e1bedd2c01e5216746df
Siqi Ren <siqi.ren@gmail.com>
2020-01-15 11:49:12 -0500

    binary search tree with insert/contains functions

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,44 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (data === this.data) {
+      return this; // because we want to return the entire node
+    } else if (data < this.data) {
+      if (this.left) {
+        return this.left.contains(data); // don't forget to *RETURN*
+      } else {
+        return null;
+      }
+    } else if (data > this.data) {
+      if (this.right) {
+        return this.right.contains(data); // don't forget to *RETURN*
+      } else {
+        return null;
+      }
+    }
+
+    return null;
+  }
+}
 
 module.exports = Node;
