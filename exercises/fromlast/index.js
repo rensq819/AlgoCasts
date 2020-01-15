709629111be7c80a47fd2c098128590bbb94bbeb
Siqi Ren <siqi.ren@gmail.com>
2020-01-14 23:21:38 -0500

    fromlast using slow-fast solution

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,25 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.head;
+  if (!slow) {
+    return null;
+  }
+
+  let fast = list.head;
+  for (let i = 0; i < n; i++) {
+    if (fast.next) {
+      fast = fast.next;
+    }
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
